6f6b6aa5cf3ccee7ea29ea533e67f32a96df1ecc
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-01-07 15:36:05 -0600

    add arreglo

---
+++ Arrays.js
@@ -0,0 +1,19 @@
+// Apply the correct Array method for removing the last item of the vegetables array.
+let vegetables = ["Tomato", "Carrot", "Broccoli"];
+vegetables.pop();
+console.log(vegetables);
+
+// Apply the correct Array method for adding “Broccoli” to the vegetables array.
+let vegetables1 = ["Tomato", "Carrot", "Garlic"];
+vegetables1.push("Broccoli");
+console.log(vegetables1);
+
+// Apply the splice()method for removing “Carrot” and “Tomato” from the vegetables array.
+let vegetables2 = ["Garlic", "Carrot", "Tomato", "Broccoli"];
+vegetables2.splice(1,2)
+console.log(vegetables2);
+
+// Apply the correct Array method for sorting the vegetables array alphabetically.
+let vegetables3 = ["Garlic", "Carrot", "Broccoli", "Pumpkin"];
+vegetables3.sort();
+console.log(vegetables3);
